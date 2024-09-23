cd0f46714a788041dcbfb89f6e403e9a892b5f24
Rosa Izquierdo <i202212506@cibertec.edu.pe>
2024-09-22 23:37:50 -0500

    Creacion de DTO

--- src/main/java/pe/edu/cibertec/rueditas_t1_front/dto/PlacaRequestDTO.java
+++ src/main/java/pe/edu/cibertec/rueditas_t1_front/dto/PlacaRequestDTO.java
@@ -1,4 +1,4 @@
 package pe.edu.cibertec.rueditas_t1_front.dto;
 
-public record BusquedaRequestDTO() {
+public record PlacaRequestDTO(String placa) {
 }
--- src/main/java/pe/edu/cibertec/rueditas_t1_front/dto/PlacaResponseDTO.java
+++ src/main/java/pe/edu/cibertec/rueditas_t1_front/dto/PlacaResponseDTO.java
@@ -1,2 +1,4 @@
-package pe.edu.cibertec.rueditas_t1_front.dto;public record PlacaResponseDTO() {
+package pe.edu.cibertec.rueditas_t1_front.dto;
+
+public record PlacaResponseDTO(String codigo, String mensaje, String modelo,int nroAsientos, double precio, String color) {
 }
